dc412b5fb9c90b261d9bd29efafa316b6fed18bc
Pratik1200 <136615342+Pratik1200@users.noreply.github.com>
2023-06-16 21:54:03 +0530

    Add files via upload

---
+++ Operators.py
@@ -0,0 +1,49 @@
+#Arithmatic Operators
+a=50
+b=30
+#Addition 
+result = a+b
+print ("Addition:",result)
+
+#Subtraction 
+result = a-b
+print ("Subtaction:",result)
+
+#Assigment operator
+#floor divison assigment 
+a = 100
+a //= 23
+print ("floor divison:",b)
+
+#Comparison Operator 
+#Equal to 
+a = 100 
+b = 50
+result = a == b
+print ("Equal to:",result)
+
+#logical operator 
+#logical AND
+a = True
+b = True
+result = a and b
+print ("Logical AND:",result)
+
+#Bitwise Operator
+#Bitwise Left shift
+a = 10 
+b = 7
+result = a << 1
+print ("Left shift:",result)
+
+#Identity Operator 
+a = 100
+b = 100
+result = a is b
+print ("Identity is:",result) 
+
+#Membership Operator
+#in operator
+string = "Batman"
+result = 'D' in string
+print ("in:",result)
